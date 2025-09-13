f5166cd5a4e3be93c95a0ccf33b39d3ebd127f0d
jmfafnir <joma.ramirez.cruz@outlook.com>
2025-09-13 14:52:33 -0500

    Se crean los interfaces

--- domain/model/src/main/java/co/com/franchises/model/product/gateways/ProductRepository.java
+++ domain/model/src/main/java/co/com/franchises/model/product/gateways/ProductRepository.java
@@ -10,6 +10,5 @@ public interface ProductRepository {
 
     Mono<Product> getProduct(String productName);
     Mono<Product> upDateProduct(Product product);
-    Mono<Boolean> deleteProduct(String productName);
     Mono<Product> getStock(String productName);
 }
